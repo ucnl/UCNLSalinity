511d54d7a3d14fd330e88a0cf2c7ff30d0727a74
AlekUnderwater <aleksandr.v.dikarev@gmail.com>
2017-02-24 01:09:14 +0300

    WOD09 support

--- NODCDataReader.cs
+++ NODCDataReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 
 namespace UCNLSalinity
@@ -114,6 +115,70 @@ namespace UCNLSalinity
             return result;
         }
 
+        public static List<SDE> ReadQuarterDegreeSalinityData(string fileName, int[] profileDepths)
+        {
+            List<SDE> result = new List<SDE>();
+            try
+            {
+                double latIdx = 0;
+                double lonIdx = 0;
+
+                double lon = lonIdx + 0.5;
+                if (lon > 180)
+                    lon = lon - 360;
+
+                double lat = latIdx - 89.5;
+
+                int depthLevelIdx = 0;
+
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var splits = line.Split(" -".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                        for (int i = 0; i < splits.Length; i++)
+                        {
+
+                            double salinity = double.Parse(splits[i]);
+                            if (salinity < 99)
+                            {
+                                result.Add(new SDE(lat, lon, profileDepths[depthLevelIdx], salinity));
+                            }
+
+                            depthLevelIdx++;
+
+                            if (depthLevelIdx >= profileDepths.Length)
+                            {
+                                depthLevelIdx = 0;
+                                lonIdx += 0.25;
+
+                                if (lonIdx >= 360)
+                                {
+                                    lonIdx = 0;
+                                    latIdx++;
+                                }
+
+                                lon = lonIdx + 0.25;
+                                if (lon > 180)
+                                    lon = lon - 360;
+
+                                lat = latIdx - 89.875;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return result;
+        }
+
+
         public static Dictionary<double, Dictionary<double, Dictionary<double, double>>> ConvertToDictionary(List<SDE> salinityList)
         {
             Dictionary<double, Dictionary<double, Dictionary<double, double>>> result = new Dictionary<double, Dictionary<double, Dictionary<double, double>>>();
@@ -164,6 +229,96 @@ namespace UCNLSalinity
 
             return result;
         }
+
+
+
+        private static int WOD09_GetColNumber(string line)
+        {
+            int pIdx = line.IndexOf('.');
+            int sIdx = line.IndexOf('#');
+            int result = -1;
+
+            if ((pIdx > sIdx) && (sIdx >= 0))
+            {
+                result = int.Parse(line.Substring(sIdx + 1, pIdx - sIdx - 1)) - 1;
+            }
+
+            return result;
+        }
+
+        public static List<MSDE> WOD09_SalinityData_Read(string fileName)
+        {
+            List<MSDE> result = new List<MSDE>();
+            try
+            {
+
+                //#ANNUAL SALINITY (unitless) at    0 meters depth
+                //# 1 degree lat/lon grid
+                //#Column definitions:
+                //#1. Latitude (degrees,negative=south),
+                //#2. Longitude (degrees,negative=west),
+                //#3. Depth (meters),
+                //#4. Objectively analyzed mean,
+                //#5. statistical mean,
+                //#6. standard deviation of statistical mean,
+                //#7. standard error of statistical mean,
+                //#8. objectively analyzed mean minus statistical mean,
+                //#9. objectively analyzed mean minus objectively analyzed annual mean,
+                //#10. number of grids with statistical means within radius of influence,
+                //#11. number of data used to calculate statistical mean
+
+                int latCol = -1;
+                int lonCol = -1;
+                int oanCol = -1;
+
+                double lat, lon, salinity;
+
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    String line;
+                    while (((line = sr.ReadLine()) != null) && (line.Contains("#"))) // read header
+                    {
+                        if (line.Contains("Latitude"))
+                            latCol = WOD09_GetColNumber(line);
+                        else if (line.Contains("Longitude"))
+                            lonCol = WOD09_GetColNumber(line);
+                        else if (line.Contains("Objectively analyzed mean"))
+                            oanCol = WOD09_GetColNumber(line);
+                    }
+
+                    if ((latCol >= 0) &&
+                        (lonCol >= 0) &&
+                        (oanCol >= 0))
+                    {
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            var splits = line.Split(",".ToCharArray());
+
+                            lat = double.Parse(splits[latCol], CultureInfo.InvariantCulture);
+                            lon = double.Parse(splits[lonCol], CultureInfo.InvariantCulture);
+                            salinity = double.Parse(splits[oanCol], CultureInfo.InvariantCulture);
+
+                            result.Add(new MSDE(lat, lon, salinity));
+                        }
+
+                    }
+                    else
+                    {
+                        throw new InvalidDataException("Error parsing header of the file");
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                /// TODO: 
+                throw e; 
+            }
+
+            return result;
+        }
+
+
 
         #endregion
     }
